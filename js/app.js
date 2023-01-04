059edc5c295f961b81007e502626ff2b74c9af8b
isobelmorales <isofmorales@gmail.com>
2023-01-04 12:22:04 -0500

    Grabbed DOM elements

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// pages
+const landingPage = document.getElementById('landing-page')
+const instructionsPage = document.getElementById('instructions-page')
+const controlsPage = document.getElementById('controls-page')
+const gamePage = document.getElementById('game-page')
+
+// buttons
+const arrow1 = document.getElementById('arrow-1')
+const arrow2 = document.getElementById('arrow-2')
+const pauseButton = document.getElementById('pause-button')
+const restartButton = document.getElementById('restart-button')
+
+// displays
+const scoreDisplay = document.getElementById('score-display')
+const levelDisplay = document.getElementById('level-display')
+const linesDisplay = document.getElementById('lines-display')
+const highscoreDisplay = document.getElementById('highscore-display')
+const gameoverDisplay = document.getElementById('game-over')
