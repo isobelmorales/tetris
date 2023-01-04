6b870c72a0422838a2e7943fd79f634684c958e6
isobelmorales <isofmorales@gmail.com>
2023-01-04 15:20:50 -0500

    Added game pieces and started setting up canvas

--- js/app.js
+++ js/app.js
@@ -28,17 +28,41 @@ let level = 1
 let lines = 0
 let highscore = 0
 let gameStatus = 'playing'
+let currentBlock = null
 
 // setting game board
-const boardWidth = 12
-const boardHeight = 20
+const squaresAcross = 12
+const squaresDown = 20
 const startX = 4
 const startY = 0
 
-// create a multidimensional array for square coordinates
-let boardCoordinates = [...Array(boardHeight)].map(e => Array(boardWidth).fill(0))
+// game pieces
+const zBlock = [[0,0], [1,0], [1,1], [2,1]]
+const sBlock = [[1,0], [2,0], [0,1], [1,1]]
+const oBlock = [[0,0], [1,0], [0,1], [1,1]]
+const lBlock = [[2,0], [0,1], [1,1], [2,1]]
+const tBlock = [[1,0], [0,1], [1,1], [2,1]]
+const iBlock = [[0,0], [1,0], [2,0], [3,0]]
+const rBlock = [[0,0], [0,1], [1,1], [2,1]]
+const gamePieces = [zBlock, sBlock, oBlock, lBlock, tBlock, iBlock, rBlock]
+
+// create a multidimensional array for square coordinates - consists of 20 items, each items is an array with 12 items
+const boardCoordinates = [...Array(squaresDown)].map(e => Array(squaresAcross).fill(0))
 
 console.log(boardCoordinates)
+
+// coordinates class
+class blockCoordinates {
+    constructor(x,y){
+        this.x = x
+        this.y = y
+    }
+}
+
+// create game board
+const setupCanvas = () => {
+    
+}
 
 // pause game
 
@@ -65,6 +89,7 @@ arrow1.addEventListener('click', () => {
 arrow2.addEventListener('click', () => {
     controlsPage.classList.toggle('hide')
     gamePage.classList.toggle('hide')
+    setupCanvas()
 })
 
 // pauseButton.addEventListener('click', )
